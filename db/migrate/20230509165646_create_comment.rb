c50a942f8dbb2c1a39f56db51b19c135f6c1ad56
otmanTR <r.brk.otman@gmail.com>
2023-05-09 20:01:22 +0300

    Add migrations

---
+++ db/migrate/20230509165646_create_comment.rb
@@ -0,0 +1,11 @@
+class CreateComment < ActiveRecord::Migration[7.0]
+  def change
+    create_table :comments do |t|
+      t.references :user_id, null: false, foreign_key: { to_table: :users }, index: true
+      t.references :post, null: false, foreign_key: true, index: true
+      t.text :text
+
+      t.timestamps
+    end
+  end
+end
