a6f0eae5948c5ee9a59b6fa5e8d14c68be582f42
otmanTR <r.brk.otman@gmail.com>
2023-05-10 16:02:32 +0300

    Check linters

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,12 +1,12 @@
 class Comment < ApplicationRecord
-    belongs_to :author, class_name: :User
-    belongs_to :post
+  belongs_to :author, class_name: :User
+  belongs_to :post
 
-    after_create :update_comment_counter
+  after_create :update_comment_counter
 
-private
+  private
 
-    def update_comment_counter
-        post.increment!(:comments_counter)
-        end
+  def update_comment_counter
+    post.increment!(:comments_counter)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,17 +1,17 @@
 class Post < ApplicationRecord
-    belongs_to :author, class_name: :User
-    has_many :likes
-    has_many :comments
+  belongs_to :author, class_name: :User
+  has_many :likes
+  has_many :comments
 
-    after_create :update_post_counter
+  after_create :update_post_counter
 
-    def update_post_counter
+  def update_post_counter
     author.increment!(:posts_counter)
-    end
+  end
 
-    def recent_five_comments
-        comments.order(created_at: :desc).limit(5)
-    end
+  def recent_five_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 
-    private :update_post_counter
+  private :update_post_counter
 end
