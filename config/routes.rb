3b75e794363fe4b53fa32b20d4237629da028cd9
otmanTR <r.brk.otman@gmail.com>
2023-05-18 19:41:17 +0300

    Add create new post functionalty

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show, :new, :create] 
+    
   end
 end
