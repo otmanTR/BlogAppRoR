964d40e9a47790323ef356453577b94e7c414dfe
otmanTR <r.brk.otman@gmail.com>
2023-05-23 22:03:33 +0300

    Remove current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
-  def current_user
-    @current_user = User.first
-  end
+  protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :photo, :bio) }
+
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :email, :photo, :bio, :password, :current_password)
+    end
 end
