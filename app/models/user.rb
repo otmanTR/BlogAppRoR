742f1074c0c2fc106fcc03bd06b622e5098b2fe4
otmanTR <r.brk.otman@gmail.com>
2023-05-25 19:21:27 +0300

    Update the user modal

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   #  :lockable, :timeoutable, :trackable and :omniauthable :confirmable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :confirmable
          :recoverable, :rememberable, :validatable
   has_many :likes, foreign_key: :author_id
   has_many :comments, foreign_key: :author_id
